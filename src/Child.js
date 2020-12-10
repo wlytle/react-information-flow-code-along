3c366c3739ffb5a0cce1520865ca9fbecbf251a4
wlytle <wlytle89@gmail.com>
2020-12-10 12:10:42 -0700

    Done.

--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import { getRandomColor } from './randomColorGenerator.js'
+import React, { Component } from "react";
+import { getRandomColor } from "./randomColorGenerator.js";
 
 class Child extends Component {
   render() {
     return (
       <div
+        onClick={this.props.handleColorChange}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{ backgroundColor: "#FFF" }}
       ></div>
-    )
+    );
   }
 }
 
-export default Child
+export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import { getRandomColor } from './randomColorGenerator.js'
-import Child from './Child'
+import React, { Component } from "react";
+import { getRandomColor } from "./randomColorGenerator.js";
+import Child from "./Child";
 
 class Parent extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
-      color: getRandomColor()
-    }
+      color: getRandomColor(),
+    };
   }
+
+  changeColor = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
 
   render() {
     return (
-      <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+      <div className="parent" style={{ backgroundColor: this.state.color }}>
+        <Child handleColorChange={this.changeColor} />
+        <Child handleColorChange={this.changeColor} />
       </div>
-    )
+    );
   }
 }
 
-export default Parent
+export default Parent;
